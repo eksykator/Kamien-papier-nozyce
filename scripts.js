9cd85540654feb28eb97a9d7e815ebdc4ca51c9e
Marta Bobowska <martabobowska2@interia.pl>
2018-05-31 10:51:03 +0200

    Set computer move. Compare moves

--- scripts.js
+++ scripts.js
@@ -6,24 +6,87 @@ var paper = 1;
 var rock = 2;
 var scissors = 3;
 
+function playerMove(playerChoose) {
+    console.log(playerChoose);
+    var computerMove = randomComputerMove();
+    
+    if (playerChoose === paper) {
+        if (computerMove === 1) {
+            console.log("REMIS");
+        } else if (computerMove === 2) {
+            console.log("WYGRAŁEŚ");
+        } else {
+        console.log("PRZEGRAŁEŚ!")
+        }
+            
+    } else if (playerChoose === rock) {
+        if (computerMove === 2) {
+            console.log("REMIS");
+        } else if (computerMove === 1) {
+            console.log("WYGRAŁEŚ!"); 
+        } else {
+            console.log("PRZEGRAŁEŚ!");
+        }
+    
+    } else {
+        if (computerMove === 3) {
+            console.log("REMIS");
+        } else if (computerMove === 1) {
+            console.log("WYGRAŁEŚ!"); 
+        } else {
+            console.log("PRZEGRAŁEŚ!")
+        }
+    }                                                            
+};
+
+function randomComputerMove()
+{
+    var computerMove = Math.round(Math.random() * 2 + 1);
+    return computerMove;
+}
 
 paperButton.addEventListener('click', function()
 {
     playerMove(paper);
+    
+    /*var computerMove = randomComputerMove();
+    
+    if (computerMove === 1) {
+        console.log("REMIS");
+    } else if (computerMove === 2) {
+       console.log("WYGRAŁEŚ!"); 
+    } else {
+        console.log("PRZEGRAŁEŚ!")
+    }*/
     
 });
 
 rockButton.addEventListener('click', function()
 {
     playerMove(rock);
+   /* var computerMove = randomComputerMove();
+    
+    if (computerMove === 2) {
+        console.log("REMIS");
+    } else if (computerMove === 1) {
+       console.log("WYGRAŁEŚ!"); 
+    } else {
+        console.log("PRZEGRAŁEŚ!")
+    }*/
 });
 
 scissorsButton.addEventListener('click', function()
 {
     playerMove(scissors);
+    /*var computerMove = randomComputerMove();
+    
+    if (computerMove === 3) {
+        console.log("REMIS");
+    } else if (computerMove === 1) {
+       console.log("WYGRAŁEŚ!"); 
+    } else {
+        console.log("PRZEGRAŁEŚ!")
+    }*/
 });
 
 
-function playerMove(playerChoose) {
-    console.log(playerChoose);
-};
